69cb15f94f9ad7adba9017e5e67c2b83bee89c76
ch4rlien <ch4rlien@gmail.com>
2015-03-04 19:30:33 +0100

    Change libraries for dragging items

--- application/resources/js/app/component/draggableOne/DraggableComponent.js
+++ application/resources/js/app/component/draggableOne/DraggableComponent.js
@@ -4,10 +4,22 @@ define([
 ], function (app, template) {
     app
         .controller('DraggableController', function($scope){
-            $scope.onItemDrop = function (event, ui) {
-                //ui.draggable.draggable();
-                //ui.draggable.draggable('option', 'revert', false)
-            }
+            $scope.dragControlListeners = {
+                accept: function (sourceItemHandleScope, destSortableScope) {
+                    console.log('asfasf')
+                    return true;
+                },
+                itemMoved: function (event) {//Do what you want
+                    console.log('asfasf')
+                },
+                orderChanged: function (event) {//Do what you want
+                    console.log('asfasf')
+                },
+                containment: '#board'//optional param.
+            };
+            $scope.items = [{
+                text: 'asdf'
+            }]
         })
         .directive('draggableOne', function(){
             return {
--- application/resources/js/app/main.js
+++ application/resources/js/app/main.js
@@ -2,12 +2,12 @@
  * Basic config for whole application. We could change the config files and include them in this one, but this file
  * might get too big.
  */
-define(['angular', 'angular-ui.router', 'uiBootstrap', 'angular-cookies', 'angular-dragdrop'], function (angular) {
+define(['angular', 'angular-ui.router', 'uiBootstrap', 'ngSortable', 'angular-cookies'], function (angular) {
 	'use strict';
 	return angular.module('app', [
 		'ui.router',
 		'ui.bootstrap',
 		'ngCookies',
-        'ngDragDrop'
+        'ui.sortable'
 	]);
 });
